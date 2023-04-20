cee7eae130466dd58cbaa255cc5c1b65cd7b7dd8
Cory Levinson <cjlevinson@gmail.com>
2023-04-20 15:04:47 -0700

    remove manual inserts from indexer_start docker script

--- docker/indexer_start.sh
+++ docker/indexer_start.sh
@@ -3,10 +3,5 @@ for file in migrations/*; do
     psql "$DATABASE_URL" -f "${file}"
 done
 
-# Workaround
-psql "$DATABASE_URL" -c "INSERT INTO chain (num, chain_id) VALUES (0, 'regen-local');"
-psql "$DATABASE_URL" -c "INSERT INTO block (chain_num, height, data, time) VALUES (0, 0, '{}', NOW());"
-
-
 # Start indexer
 python index.py
