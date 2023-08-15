156782744780c9c659da383d0a358a4e2c459626
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2023-08-15 11:00:46 -0400

    feat: index tx.data tx_response code and type

---
+++ sql/V1_9__all_ecocredit_txes.sql
@@ -0,0 +1,16 @@
+CREATE INDEX IF NOT EXISTS tx_data_tx_response_code_idx ON tx USING GIN ((DATA -> 'tx_response' -> 'code'));
+
+CREATE INDEX IF NOT EXISTS msg_event_type_idx ON msg_event (
+  TYPE
+);
+
+DROP FUNCTION IF EXISTS all_ecocredit_txes;
+
+CREATE FUNCTION all_ecocredit_txes () RETURNS SETOF tx AS $$
+    select tx.*
+    from tx
+    natural join msg_event as me
+    where
+      data ->'tx_response'->'code' @> '0'
+      and me.type like 'regen.ecocredit.%'
+$$ LANGUAGE SQL STABLE;
